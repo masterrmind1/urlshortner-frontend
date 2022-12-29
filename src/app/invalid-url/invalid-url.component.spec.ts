94fd92ac4bf1811135f64a74a2cddbc69bfaa1da
Masterrmind1 <rajputp2007@gmail.com>
2022-12-29 14:20:56 +0530

    Save uncommited changes

---
+++ src/app/invalid-url/invalid-url.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InvalidUrlComponent } from './invalid-url.component';
+
+describe('InvalidUrlComponent', () => {
+  let component: InvalidUrlComponent;
+  let fixture: ComponentFixture<InvalidUrlComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InvalidUrlComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InvalidUrlComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
